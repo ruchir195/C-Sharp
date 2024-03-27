9f18bbb937328f4e8769ca00873aec086e299b94
ruchir195 <ruchirgparmar@gmail.com>
2024-03-27 22:54:38 +0530

    1) Nullable    2) Nullable methods    3) indexer

---
+++ Practical-3/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_3
+{
+
+    class A
+    {
+
+        // indexer
+        // indexer use for index type value like array
+        // indexer is same as property but property use normal datatype int, float, string....
+        // indexer ka name nahi hota                                     // property ka name hot ahi
+        // indexer me parameter pass kar sakte hai                       // property me parameter pass nahi kar sakte
+        private string[] names = new string[5];
+
+        public string this[int i]              // this kyword is use
+        {
+            get { return names[i]; }            // same property use get and set method
+
+            set { names[i] = value; }
+        }
+
+
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Nullable type
+            Console.WriteLine("\n initialize nullable type");
+            Nullable<int> x;           // nullable type declaration
+
+            int? s = null;             // another way to declare nullable type
+            Console.WriteLine("s = " + s);
+
+
+            Console.WriteLine("\n HasValue method");
+            if (s.HasValue)                 // HasValue method to check value is not null then enter into if conditions
+            {
+                Console.WriteLine("s not null value");
+            }
+            else                             // if value is null then enter into else condition
+            {
+                Console.WriteLine("s null value");
+            }
+
+            int? s1 = 23;
+            Console.WriteLine("\ns1 = " + s1);
+            if (s1.HasValue)
+            {
+                Console.WriteLine("s1 not null value");
+            }
+            else
+            {
+                Console.WriteLine("s1 null value");
+            }
+
+
+
+
+            // GetValueOrDefault method
+            Console.WriteLine("\nGetValueOrDefault method");
+            int? s4 = 20;
+            int? s5 = null;
+            int m3 = s4.GetValueOrDefault();
+            Console.WriteLine("m3 = "+m3);
+            int m4 = s5.GetValueOrDefault();
+            Console.WriteLine(" m4 = "+m4);
+
+
+            //Null - Coalescing Operator
+            Console.WriteLine("\nNull coalescing operator");
+            int? s2 = 34;
+            int m = s2 ?? 24;                      // if value null nahi hogi to print 32
+            Console.WriteLine("m = " + m);         // but value null then print default value means 24
+
+            int? s3 = null;
+            int m2 = s3 ?? 25;
+            Console.WriteLine("m2 = " + m2);
+
+
+
+
+            // Nullable.compare method
+            // Nullable.compare method ka use null or normal value ko compare karne ke liye hota hai 
+            Console.WriteLine("\n Nullable.compare method");
+            int? a = null;                             // if null,10 then (a,b) < 0
+            int? b = 10;                               // if 10,null then (a,b) > 0
+                                                       // if null,null then (a,b) = 0
+            if(Nullable.Compare<int>(a,b) < 0)
+            {
+                Console.WriteLine("(a,b) < 0");
+            }
+            else if(Nullable.Compare(a,b) > 0)
+            {
+                Console.WriteLine("(a,b) > 0");
+            }
+            else
+            {
+                Console.WriteLine("(a, b) = 0");
+            }
+
+
+
+
+            // indexer
+            // object
+            A a1 = new A();
+
+            Console.WriteLine("\nIndexer");
+            // in object pass array value means indexer
+            a1[0] = "Ruchir";
+            a1[1] = "Parmar";
+            a1[2] = "Girshbhai";
+            a1[3] = "Laxman";
+            a1[4] = "Primal";
+
+
+            // show the array value
+            for (int i = 0; i < 5 ; i++)
+            {
+                Console.WriteLine(a1[i]);
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
--- practice-2/Program.cs
+++ practice-2/Program.cs
@@ -1,45 +1,284 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace practice_2
 {
-
+    // normal class
     class A
     {
-        int a, b;
+        int a;
+        int c;
 
         public A()
         {
-
+           
         }
+
+        public A(int a)
+        {
+            this.a = a;
+        }
+
+        public static int b = 20;
 
         public void Show(int a)
         {
+            Console.WriteLine("\n normal method");
             Console.WriteLine("a = " + a);
+            Console.WriteLine("Static variable");
+            Console.WriteLine("b = " + b);
         }
+
+
+        
+
+        public static void Show2(int a)
+        {
+            Console.WriteLine("\n static method");
+            Console.WriteLine(" a = " + a);
+            Console.WriteLine("static variable");
+            Console.WriteLine(" b = " + b);
+           // Console.WriteLine(" c = " + c);            // give error because normal member are not access by static method
+        }
+
+
+        static int x = 20;
+        static int y;
+
+        static A()
+        {
+            Console.WriteLine("\n static constructor called");                                                                                                                                    
+            y = x + 10;
+            Console.WriteLine("x = " + x + " y = " + y);
+        }
+
+
+
+        // property
+        private int k = 10;
+       public int K
+        {
+            get { k = k + 10; return k; }
+            set { k = value; }
+        }
+
+
+        // static property
+        private static int m = 10;
+        public static int M
+        {
+            get { m = m * 10; return m; }
+            set { m = value; }
+        }
+
+
+
+        // read only property
+        private String str = "Ruchir";
+        public String N
+        {
+            get { return str; }
+        }
+
+
+        // write only property
+
+        private String str2;
+        public String O
+        {
+            set { str2 = value; }
+        }
+
+        public int X { get; set; }
+        public float Y { get; set; }
+        public String Z { get; set; }
+
+
+        const int p = 10;
+        public void Display2()
+        {
+            Console.WriteLine("\n const keyword");
+            Console.WriteLine("p = " + p);
+        }
+
+
+        public readonly int r;
+        public readonly int s = 20;
+        public static readonly int t = 30;
+
+
+        public A(int a, int b)
+        {
+            Console.WriteLine("r = " + r);
+            Console.WriteLine("s = " + s);
+            Console.WriteLine("t = " + t);
+        }
+
+        
+
+
+
+
+        
+    }
+
+    // static class 
+    static class B
+    {
+       // int a; // give error 
+        static int a;
+
+        static B()
+        {
+            Console.WriteLine("\n static class constructor");
+        }
+
 
         public static void Show2(int a)
         {
             Console.WriteLine("a = " + a);
         }
-
-        
     }
+
+
+
+
+    class C
+    {
+        public void Show(ref int a, ref int b)
+        {
+            Console.WriteLine("\n ref keyword");
+            Console.WriteLine("a = " +a);
+            Console.WriteLine("b = " +b);
+        }
+
+        public void Show2(out int a, out int b)
+        {
+            a = 5;
+            b = 10;
+            Console.WriteLine("\n out keyword");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            A a = new A(10);
 
-            A a = new A();
-            a.Show(12);
+            // normal method call
+            a.Show(20);
 
-            A.Show2(23);
+
+            // static method call
+            A.Show2(20);
+
+            // static class method call
+            B.Show2(30);
+
+            // property
+            Console.WriteLine("\n property");
+            Console.WriteLine(a.K);
+            a.K = 20;
+
+
+            // static property
+            Console.WriteLine("\n static property");
+            Console.WriteLine(A.M);
+            A.M = 30;
+
+            //read only property
+            Console.WriteLine("\n read only property");
+            Console.WriteLine(a.N);
+
+
+            //write only property
+            Console.WriteLine("\n write only property");
+            a.O = "Parmar";
+            Console.WriteLine("\n value set successfully done");
+
+
+            // object initialize
+            Console.WriteLine("\n object initialize");
+            A a2 = new A()
+            {
+                X = 100,
+                Y = 12.5f,
+                Z = "Ruchir"
+            };
+
+            Console.WriteLine(" a2.x = " + a2.X);
+            Console.WriteLine(" a2.y = " + a2.Y);
+            Console.WriteLine(" a2.z = " + a2.Z);
+
+            a.Display2();
+
+
+            // readonly keyword
+            Console.WriteLine("\nreadonly keyword");
+            A a3 = new A(10, 20);
+
+
+
+            C c = new C();
+
+            // call by value
+            int d = 40;
+            int f = 50;
+           // c.Show3(int d, int f);
+
+            // call by ref
+            int p = 20;
+            int q = 30;
+            c.Show(ref p, ref q);
+            Console.WriteLine("\n in main method");
+            Console.WriteLine(" p = " + p);
+            Console.WriteLine(" q = " + q);
+
+
+            // call by out
+            int r, s;
+            c.Show2(out r, out s);
+            Console.WriteLine("\n in main method");
+            Console.WriteLine(" r = " + r);
+            Console.WriteLine(" s = " + s);
+
+
+            // var keyword
+            Console.WriteLine("\n var keyword");
+            var e = 20;
+            Console.WriteLine(e);
+            e = 12;
+            Console.WriteLine(e);
+
+            //e = "ruchir"; // give error
+
+
+            // dynamic keyword
+            Console.WriteLine("\n dynamic keyword");
+            dynamic l = 20;
+            Console.WriteLine(l);
+            l = 30;
+            Console.WriteLine(l);
+            l = "Ruchir";
+            Console.WriteLine(l);
+
+
+
+
+
+
 
             Console.Read();
         }
     }
 }
+
